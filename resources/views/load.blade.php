e772116225762e9f38e8bd02eb32f8f5bf583bb9
mackevicius1988 <mackevicius1988@gmail.com>
2017-11-06 20:17:49 +0200

    atstate

--- resources/views/load.blade.php
+++ resources/views/load.blade.php
@@ -1,18 +1,9 @@
-
-
 <ul id="og-grid" class="og-grid">
-
 @foreach($posts as $post)
-
     <li style="width: 275px; height: 275px; overflow: hidden" class="gridItem">
-
 
         <a class="animsition-link" href="{{$post->id}}" data-id="{{$post->id}}" data-price="{{$post->price}}" data-largesrc="{{$post->url}}" data-title="{{$post->name}}" data-description="{{$post->desc}}">
             <div style="position: absolute; top: 0px; left: 0px; padding: 10px; z-index: 999; background: #551313; color: white; opacity: 0.9">SALE</div>
-
-
-
-
             <div class="item" style="background: url('{{$post->url}}');
                     background-position: top center;
                     background-size:100% auto; width: 275px; height: 275px;  background-repeat: no-repeat;">
