a9fc96bc571585fd090154e29ae102d85b3b20c9
mackevicius1988 <mackevicius1988@gmail.com>
2017-11-07 17:38:47 +0200

    style

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -306,7 +306,7 @@
                         <!-- Post -->
                         <section class="blog-post" style="margin-bottom: 0">
                             <!-- Post Media -->
-                            <div class="blog-page-media">
+                            <div class="blog-page-media" style="height: 500px;">
                                <img src="{{$post->imageUrl}}" style="max-width:100%; max-height:100%;    ">
                             </div>
                             <div class="shareHolder" style="text-align: center;">
